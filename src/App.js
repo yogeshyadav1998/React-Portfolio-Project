d16715c84c8e7a76d4aef7920e5c166fd5f0f950
yogeshyadav1998 <yogeshbhakli@gmail.com>
2021-04-16 19:08:48 +0530

    header added

--- src/App.js
+++ src/App.js
@@ -1,11 +1,15 @@
 import './App.css';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import Navbar from  './components/Navbar/navbar';
+import Header from './components/Header/header';
+import About from './components/About/about';
+
 function App() {
   return (
     <div className="App">
       <Navbar/>
-      <h1>Portfolio</h1>
+      <Header/>
+      <About/>
     </div>
   );
 }
---
+++ src/components/About/about.js
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const about = () => {
+    return (
+        <div className="about">
+            <h1>My name is yogesh</h1>
+        </div>
+    )
+}
+
+    export default about; 
