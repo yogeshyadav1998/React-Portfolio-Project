18a7a32d965bb848b67f8ee02fb847bc60c2906a
yogeshyadav1998 <yogeshbhakli@gmail.com>
2021-04-16 14:59:27 +0530

    navbar added

--- src/App.js
+++ src/App.js
@@ -1,9 +1,12 @@
 import './App.css';
 import 'bootstrap/dist/css/bootstrap.min.css';
-
+import Navbar from  './components/Navbar/navbar';
 function App() {
   return (
-    <h1>Portfolio</h1>
+    <div className="App">
+      <Navbar/>
+      <h1>Portfolio</h1>
+    </div>
   );
 }
 
