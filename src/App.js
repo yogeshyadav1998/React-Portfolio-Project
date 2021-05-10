fce7a43b38a7a46cc1b477ff7de166ed6609af5f
yogeshyadav1998 <yogeshbhakli@gmail.com>
2021-05-10 13:59:58 +0530

    new face

--- src/App.js
+++ src/App.js
@@ -1,5 +1,6 @@
 import './App.css';
 import 'bootstrap/dist/css/bootstrap.min.css';
+import Particles from 'react-particles-js';
 import Navbar from  './components/Navbar/navbar';
 import Header from './components/Header/header';
 import About from './components/About/about';
@@ -9,6 +10,26 @@ import Contacts from './components/Contact/contact';
 function App() {
   return (
     <div className="App">
+      <Particles
+        params={{
+          particles:{
+            number:{
+              value:25,
+              density:{
+                enable:true,
+                value_area: 1000
+              }
+            },
+            shape:{
+              type: "circle",
+              stroke:{
+                width: 6,
+                color: "rgba(133, 41, 34, 0.911)"
+              }
+            }
+          }
+        }}
+      />
       <Navbar/>
       <Header/>
       <About/>
